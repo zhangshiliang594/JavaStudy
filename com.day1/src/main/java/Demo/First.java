e52a9b846ae53acee9c910f616889c2d71297a3c
zhangshiliang <zhangshiliang594@163.com>
2022-01-22 14:32:29 +0800

    首次提交代码

---
+++ com.day1/src/main/java/Demo/First.java
@@ -0,0 +1,12 @@
+package Demo;
+
+/**
+ * @author zhangshiliang
+ * @Description
+ * @create 2022-01-22 2:25 下午
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello Java!!!!");
+    }
+}
